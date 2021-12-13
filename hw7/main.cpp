aaf7030206e9f6346c37bb775fc1c889caf26c49
zycao29 <zycao29@gmail.com>
2021-12-13 16:32:11 -0600

    3

--- hw7/main.cpp
+++ hw7/main.cpp
@@ -10,28 +10,28 @@
 
 int main(void){
 
-  my_matrix<double> testA(2, 2, "A");
-  my_matrix<double> testB(2, 1, "B");
-  my_matrix<double> testsum = testA + testB;
+  // my_matrix<double> testA(2, 2, "A");
+  // my_matrix<double> testB(2, 1, "B");
+  // my_matrix<double> testsum = testA + testB;
 
-  my_matrix<float> C(3, 0, "C");
+  // my_matrix<float> C(3, 0, "C");
   
-  my_matrix<double> A(2, 2, "A");
-  my_matrix<double> B(2, 1, "B");
-  A(0,0) = 1;
-  A(0,1) = 0;
-  A(1,0) = 0;
-  A(1,1) = 1;
-  B(0,0) = 1;
-  B(1,0) = 2;
-  A.display();
-  B.display();
-  my_matrix<double> sol(2, 1, "sol");
-  sol = A|B;
-  sol.display();
+  // my_matrix<double> A(2, 2, "A");
+  // my_matrix<double> B(2, 1, "B");
+  // A(0,0) = 1;
+  // A(0,1) = 0;
+  // A(1,0) = 0;
+  // A(1,1) = 1;
+  // B(0,0) = 1;
+  // B(1,0) = 2;
+  // A.display();
+  // B.display();
+  // my_matrix<double> sol(2, 1, "sol");
+  // sol = A|B;
+  // sol.display();
 
-  my_matrix<double> A2(3, 3, "A2");
-  my_matrix<double> B2(3, 1, "B2");
+  // my_matrix<double> A2(3, 3, "A2");
+  // my_matrix<double> B2(3, 1, "B2");
   // ....
 
 
